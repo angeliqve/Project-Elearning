1e24b1798d0977817f5e33973cb05a6ae303e91a
angeliqve <angeliquegbriel@gmail.com>
2025-07-25 23:47:19 +0700

    init: day 3

---
+++ src/models/user-model.ts
@@ -0,0 +1,10 @@
+export interface UserData {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  tanggalLahir: Date;
+  sudahLulus: boolean;
+  skorKeseluruhan: number;
+  role: string;
+}
---
+++ src/routes/user-routes.ts
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const authenticationMiddleware = require('../middlewares/authentication-middleware');
+const onlyAdminMiddleware = require('../middlewares/only-admin-middleware');
+const userController = require('../controllers/user-controller');
+
+// GET /api/users
+router.get('/', authenticationMiddleware, onlyAdminMiddleware, userController.index);
+
+// PATCH /api/users
+router.patch('/:id', authenticationMiddleware, onlyAdminMiddleware, userController.update);
+
+// DELETE /api/users/:id -> hanya boleh oleh ADMIN
+router.delete(
+  '/:id',
+  authenticationMiddleware,
+  onlyAdminMiddleware,
+  userController.deleteById,
+);
+
+module.exports = router;
